1b62a8c5aaea246d7416fcb9cfc251f56734192d
HsiangLeekwok <xiang.l.g@gmail.com>
2017-04-13 09:44:48 +0800

    promoted 1.0.4.1

---
+++ hlklib/src/main/java/com/hlk/hlklib/layoutmanager/CustomLinearLayoutManager.java
@@ -0,0 +1,43 @@
+package com.hlk.hlklib.layoutmanager;
+
+import android.content.Context;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+
+import com.hlk.hlklib.etc.Log;
+
+/**
+ * <b>功能描述：</b><br />
+ * <b>创建作者：</b>Hsiang Leekwok <br />
+ * <b>创建时间：</b>2017/03/07 10:03 <br />
+ * <b>作者邮箱：</b>xiang.l.g@gmail.com <br />
+ * <b>最新版本：</b>Version: 1.0.0 <br />
+ * <b>修改时间：</b>2017/03/07 10:03 <br />
+ * <b>修改人员：</b><br />
+ * <b>修改备注：</b><br />
+ */
+
+public class CustomLinearLayoutManager extends LinearLayoutManager {
+
+    public CustomLinearLayoutManager(Context context) {
+        super(context);
+    }
+
+    public CustomLinearLayoutManager(Context context, int orientation, boolean reverseLayout) {
+        super(context, orientation, reverseLayout);
+    }
+
+    public CustomLinearLayoutManager(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    public void onLayoutChildren(RecyclerView.Recycler recycler, RecyclerView.State state) {
+        try {
+            super.onLayoutChildren(recycler, state);
+        } catch (IndexOutOfBoundsException e) {
+            Log.log("ERROR", e.getMessage());
+        }
+    }
+}
---
+++ hlklib/src/main/java/com/hlk/hlklib/layoutmanager/CustomStaggeredGridLayoutManager.java
@@ -0,0 +1,39 @@
+package com.hlk.hlklib.layoutmanager;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.util.AttributeSet;
+
+import com.hlk.hlklib.etc.Log;
+
+/**
+ * <b>功能描述：</b><br />
+ * <b>创建作者：</b>Hsiang Leekwok <br />
+ * <b>创建时间：</b>2017/03/07 10:07 <br />
+ * <b>作者邮箱：</b>xiang.l.g@gmail.com <br />
+ * <b>最新版本：</b>Version: 1.0.0 <br />
+ * <b>修改时间：</b>2017/03/07 10:07 <br />
+ * <b>修改人员：</b><br />
+ * <b>修改备注：</b><br />
+ */
+
+public class CustomStaggeredGridLayoutManager extends StaggeredGridLayoutManager {
+
+    public CustomStaggeredGridLayoutManager(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    public CustomStaggeredGridLayoutManager(int spanCount, int orientation) {
+        super(spanCount, orientation);
+    }
+
+    @Override
+    public void onLayoutChildren(RecyclerView.Recycler recycler, RecyclerView.State state) {
+        try {
+            super.onLayoutChildren(recycler, state);
+        } catch (IndexOutOfBoundsException e) {
+            Log.log("ERROR", e.getMessage());
+        }
+    }
+}
