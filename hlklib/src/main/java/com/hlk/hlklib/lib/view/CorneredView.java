1f06ad8bcc8fec82af46385d94d5ede5aed79762
HsiangLeekwok <xiang.l.g@gmail.com>
2017-04-18 10:11:49 +0800

    更改CorneredView的背景处理

--- hlklib/src/main/java/com/hlk/hlklib/lib/view/CorneredView.java
+++ hlklib/src/main/java/com/hlk/hlklib/lib/view/CorneredView.java
@@ -8,6 +8,7 @@ import android.graphics.drawable.GradientDrawable;
 import android.graphics.drawable.StateListDrawable;
 import android.os.Build;
 import android.util.AttributeSet;
+import android.view.View;
 import android.widget.LinearLayout;
 
 import com.hlk.hlklib.R;
@@ -22,7 +23,7 @@ import com.hlk.hlklib.etc.Utility;
 public class CorneredView extends LinearLayout {
 
     // setting
-    private int actbd_color, dftbd_color, dsbbd_color, bg_color, bg_act, corner, lftt, lftb, ritt, ritb, border, bg_type;
+    private int actBorder, dftBorder, dsbBorder, bgNormal, bgActive, corner, lftt, lftb, ritt, ritb, border, bgType;
 
     public CorneredView(Context context) {
         this(context, null);
@@ -42,15 +43,15 @@ public class CorneredView extends LinearLayout {
     }
 
     private void getAttributes(TypedArray array) {
-        actbd_color = array.getColor(R.styleable.CorneredView_cv_active_border, Default.ACT_COLOR);
-        dftbd_color = array.getColor(R.styleable.CorneredView_cv_normal_border, Default.DFT_COLOR);
-        dsbbd_color = array.getColor(R.styleable.CorneredView_cv_disabled_border, Default.DSB_COLOR);
+        actBorder = array.getColor(R.styleable.CorneredView_cv_active_border, Default.ACT_COLOR);
+        dftBorder = array.getColor(R.styleable.CorneredView_cv_normal_border, Default.DFT_COLOR);
+        dsbBorder = array.getColor(R.styleable.CorneredView_cv_disabled_border, Default.DSB_COLOR);
 
-        bg_color = array.getColor(R.styleable.CorneredView_cv_background, Color.WHITE);
+        bgNormal = array.getColor(R.styleable.CorneredView_cv_background, Color.WHITE);
         border = array.getDimensionPixelOffset(R.styleable.CorneredView_cv_border_size, Default.BORDER);
         corner = array.getDimensionPixelOffset(R.styleable.CorneredView_cv_corner_size, 0);
-        bg_type = array.getInt(R.styleable.CorneredView_cv_background_type, TYPE_BORDER);
-        bg_act = array.getColor(R.styleable.CorneredView_cv_background_active, Default.ACT_COLOR);
+        bgType = array.getInt(R.styleable.CorneredView_cv_background_type, TYPE_BORDER);
+        bgActive = array.getColor(R.styleable.CorneredView_cv_background_active, Default.ACT_COLOR);
         if (corner > 0) {
             lftt = corner;
             lftb = corner;
@@ -66,8 +67,17 @@ public class CorneredView extends LinearLayout {
 
     private Drawable getDrawable(int color, int state) {
         GradientDrawable gd = new GradientDrawable();
-        gd.setStroke(border, color);
-        gd.setColor(bg_type == TYPE_FILL ? color : bg_color);
+        // 边框
+        if (border > 0) {
+            gd.setStroke(border, color);
+        }
+        // 背景色
+        if (bgType == TYPE_BORDER) {
+            // 边框模式只显示不同颜色的边框，内容只是默认的背景不变
+            gd.setColor(bgNormal);
+        } else {
+            gd.setColor(state > 0 ? bgActive : (state < 0 ? bgNormal : dsbBorder));
+        }
         gd.setCornerRadius(corner);
         if (lftt != corner || ritt != corner || lftb != corner || ritb != corner) {
             gd.setCornerRadii(new float[]{lftt, lftt, ritt, ritt, ritb, ritb, lftb, lftb});
@@ -77,8 +87,8 @@ public class CorneredView extends LinearLayout {
 
     @SuppressWarnings("deprecation")
     private void init() {
-        Drawable active = getDrawable(actbd_color, 1),
-                normal = getDrawable(dftbd_color, 0), disabled = getDrawable(dsbbd_color, -1);
+        Drawable active = getDrawable(actBorder, 1),
+                normal = getDrawable(dftBorder, -1), disabled = getDrawable(dsbBorder, 0);
         StateListDrawable sld = new StateListDrawable();
         sld.addState(new int[]{-android.R.attr.state_enabled}, disabled);
         sld.addState(new int[]{android.R.attr.state_pressed}, active);
@@ -93,11 +103,28 @@ public class CorneredView extends LinearLayout {
         invalidate();
     }
 
+    @Override
+    public void setOnClickListener(OnClickListener l) {
+        _customListener = l;
+        super.setOnClickListener(_defaultListener);
+    }
+
+    private OnClickListener _customListener;
+
+    private OnClickListener _defaultListener = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if (null != _customListener) {
+                _customListener.onClick(v);
+            }
+        }
+    };
+
     /**
      * 设置背景颜色
      */
     public void setBackground(int color) {
-        bg_color = color;
+        bgNormal = color;
         init();
     }
 
@@ -113,7 +140,7 @@ public class CorneredView extends LinearLayout {
      * 设置Active时的边框颜色
      */
     public void setActiveColor(int color) {
-        actbd_color = color;
+        actBorder = color;
         init();
     }
 
@@ -121,12 +148,12 @@ public class CorneredView extends LinearLayout {
      * 设置默认边框颜色
      */
     public void setNormalColor(int color) {
-        dftbd_color = color;
+        dftBorder = color;
         init();
     }
 
     public void setDisabledColor(int color) {
-        dsbbd_color = color;
+        dsbBorder = color;
         init();
     }
 
@@ -134,11 +161,9 @@ public class CorneredView extends LinearLayout {
     public static final int TYPE_BORDER = 1;
 
     private static class Default {
-        public static final int ACT_COLOR = Color.parseColor("#FF8909");
-        public static final int DFT_COLOR = Color.parseColor("#2cc3bb");
-        public static final int DSB_COLOR = Color.parseColor("#e7e7e7");
-        public static final int CORNER = Utility.ConvertDp(5);
-        public static final int BORDER = Utility.ConvertDp(1);
-        private static final int BGTYPE = TYPE_FILL;
+        static final int ACT_COLOR = Color.parseColor("#FF8909");
+        static final int DFT_COLOR = Color.parseColor("#2cc3bb");
+        static final int DSB_COLOR = Color.parseColor("#e7e7e7");
+        static final int BORDER = Utility.ConvertDp(1);
     }
 }
