fbdcf22f48cd8963673b4d3517b66cf2e0c40baf
HsiangLeekwok <xiang.l.g@gmail.com>
2018-12-06 14:20:48 +0800

    ClearEditText 重设 maxLength 时，修复显示的输入统计不正确的问题

--- hlklib/src/main/java/com/hlk/hlklib/lib/view/ClearEditText.java
+++ hlklib/src/main/java/com/hlk/hlklib/lib/view/ClearEditText.java
@@ -431,7 +431,8 @@ public class ClearEditText extends RelativeLayout {
     }
 
     public void setMaxLength(int maxLength) {
-        editTextView.setMaxLength(maxLength);
+        editMaxLen = maxLength;
+        editTextView.setMaxLength(editMaxLen);
     }
 
     public void setMaxLines(int maxLines) {
